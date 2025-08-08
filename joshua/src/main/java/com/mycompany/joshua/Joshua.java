d936d208aa1016b732653c48627192398ca0e55f
CL1-PC33 <CL1-PC33@DESKTOP-4SVPNK0>
2025-08-08 10:00:58 +0800

    8825

---
+++ joshua/src/main/java/com/mycompany/joshua/Joshua.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.joshua;
+
+/**
+ *
+ * @author CL1-PC33
+ */
+public class Joshua {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
